e9f61a6ec39f36d9db6f3e35a32f6798e5eb9b58
conferenceal <appconferenceal@gmail.com>
2025-10-25 14:14:56 +0700

    Component

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -2,10 +2,19 @@ function HelloWorld() {
   return (
     // NOTED : Gunakan tag <> </> (fragment) atau tag <div></div> untuk diawal
     <>
-      <h1>Hello World</h1>
-      <p>Selamat Belajar ReactJS dari Programmer Zaman Now</p>
+      {/* Multiple Component */}
+      <HeaderHelloWorld />
+      <ParagraphHelloWorld />
     </>
   );
 }
 
 export default HelloWorld;
+
+function HeaderHelloWorld() {
+  return <h1>Hello World</h1>;
+}
+
+function ParagraphHelloWorld() {
+  return <p>Selamat Belajar ReactJS dari Programmer Zaman Now</p>;
+}
